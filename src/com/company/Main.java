a43ae166f1ffed82b5877c101aa526c269bcf302
Shinkary <50045857+Shinkary@users.noreply.github.com>
2019-04-27 15:40:06 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,17 @@ public class Main {
         // write your code here
         System.out.println("Используя этот онлайн калькулятор для решения квадратных уравнений, вы сможете очень просто и быстро найти корни квадратного уравнения.");
         System.out.println("ax\u00b2+bx+c=0");
-        //UserNumber userNumber = new UserNumber(); //получение данных от пользователя
+        //UserNumber userNumber = new UserNumber(); //можно без инициализации объекта?
         double a = UserNumber.inputNumber("a");
 
-        if (a != 0) {
+        if (a != 0) { //проверка на первый коэффициент
 
             double b = UserNumber.inputNumber("b");
             double c = UserNumber.inputNumber("c");
 
 
-
-            if (b != 0 && c != 0){
+            //приятный вывод уравнения для пользователя
+            if (b != 0 && c != 0){ 
                 System.out.println(a + "x\u00b2" + b + "x" + c + "=0");
             }
             else if (b == 0){
@@ -32,7 +32,8 @@ public class Main {
             double discr = b * b - 4 * a * c; //определение дискриминанта
 
             System.out.println("Вычисление дискриминанта по формуле: D=" + b + "\u00b2+4*" + a + "*" + c);
-
+            
+            //вычисление корней
             if (discr > 0) {
                 double x1 = (-b + Math.sqrt(discr)) / (2 * a);
                 double x2 = (-b - Math.sqrt(discr)) / (2 * a);
